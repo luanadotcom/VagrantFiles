455e535f8400a50ab7a35a3c52ebe5aca9f1cc00
Luana Gouvea <luana.gouvea@tigerspike.com>
2017-07-30 15:57:02 +0100

    Corrected the configurations to be installed as provision

--- provision.sh
+++ provision.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
+#
+# Vagrant CentOS 7 machine provisioner - installs useful packages and Jenkins
+# Luana Gouvea
+# 30-Jul-2017
 
 echo "Installing Jenkins and useful packages - please wait"
 
 # useful
-sudo yum update >/dev/null 2>&1
-sudo yum install wget vim bash-completion -y
+echo "==============================================="
+echo "Updating the system"
+echo "==============================================="
+sudo yum update
+
+echo "==============================================="
+echo "Installing useful packages"
+echo "==============================================="
+sudo yum install wget vim bash-completion java -y
 
 # Installing Jenkins
+echo "==============================================="
+echo "Installing Jenkins and setting it up"
+echo "==============================================="
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
-sed -i 's/8080/9000/g' /etc/sysconfig/jenkins
+sudo yum install jenkins -y
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
